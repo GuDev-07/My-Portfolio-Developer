6b732d391d8e220c02d251eb9876b6f085086ad2
Gustavo <gustavinhojesussant@gmail.com>
2025-10-14 02:56:09 -0300

    atualizando recomendações e estilizando

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
+//MENU HAMBURGUER
 const menu = document.querySelector('.menu');
 const openBtn = document.getElementById('menuToggle');
 const closeBtn = document.querySelector('.btnClose');
 const menuLinks = document.querySelectorAll('.menu-link');
 
-// Abrir menu
+//OPEN MENU
 openBtn.addEventListener('click', () => {
   menu.classList.add('active');
   openBtn.style.display = 'none';
   closeBtn.style.display = 'block';
 });
 
-// Fechar menu
+//CLOSE MENU
 closeBtn.addEventListener('click', () => {
   menu.classList.remove('active');
   openBtn.style.display = 'block';
   closeBtn.style.display = 'none';
 });
 
-// Fechar ao clicar em qualquer link
+//CLOSE ON CLICK IN LINK
 menuLinks.forEach(link => {
   link.addEventListener('click', () => {
     menu.classList.remove('active');
@@ -27,6 +28,7 @@ menuLinks.forEach(link => {
 });
 
 
+//CARROSEL DOS PROJETOS (SWIPER)
 new Swiper('.card-wrapper', {
   loop: true,
   spaceBetween: 30,
@@ -36,7 +38,7 @@ new Swiper('.card-wrapper', {
     el: '.swiper-pagination',
   },
 
-  // Navigation arrows
+  //SETAS
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
@@ -56,24 +58,7 @@ new Swiper('.card-wrapper', {
   }
 });
 
-new Swiper('.skills-swiper', {
-  loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  breakpoints: {
-    0: { slidesPerView: 1 },
-    768: { slidesPerView: 2 },
-    1024: { slidesPerView: 3 },
-  }
-});
-
-
+//ANIMATION OF METRICS COUTING
 const counters = document.querySelectorAll('.metric h3');
 
 const observer = new IntersectionObserver((entries, observer) => {
@@ -103,7 +88,7 @@ const observer = new IntersectionObserver((entries, observer) => {
 
 counters.forEach(counter => observer.observe(counter));
 
-// ===== EFEITO DE APARECER AO ROLAR =====
+//EFEITO DE APARECER AO ROLAR
 function revealOnScroll() {
     const reveals = document.querySelectorAll('.reveal');
     for (let i = 0; i < reveals.length; i++) {
@@ -118,3 +103,18 @@ function revealOnScroll() {
   }
 
   window.addEventListener('scroll', revealOnScroll);
+
+//FUNCTION SEND WHATSAPP
+function enviarWhatsApp(event){
+  event.preventDefault(); // Impede o envio tradicional do formulário
+
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const mensagem = document.getElementById("mensagem").value;
+
+  const texto = `Estou vindo do seu portfólio! Me chamo ${nome}`;
+  const numero = "5515988036921"; // Substitua pelo seu número com DDD e código do país
+  const url = `https://api.whatsapp.com/send?phone=${5515988036921}&text=${encodeURIComponent(texto)}`;
+
+  window.open(url, "_blank");
+}
