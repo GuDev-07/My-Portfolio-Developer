509ea3cd9344845a70e29b22b2bc4e439d4d968b
Gustavo <gustavinhojesussant@gmail.com>
2025-10-23 15:57:21 -0300

    registrando uma recomendação

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ function enviarWhatsApp(event){
   const email = document.getElementById("email").value;
   const mensagem = document.getElementById("mensagem").value;
 
-  const texto = `Estou vindo do seu portfólio!\nMe chamo ${nome}.\n${mensagem}`;
+  const texto = `Estou vindo do seu portfólio!\nMe chamo ${nome} e meu e-mail é ${email}.\n${mensagem}`;
   const numero = "5515988036921"; // Substitua pelo seu número com DDD e código do país
   const url = `https://api.whatsapp.com/send?phone=${5515988036921}&text=${encodeURIComponent(texto)}`;
 
